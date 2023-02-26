928d7ace727ec0783568eeccc1553a9b9550bf3a
Buğra <109425576+bugraozdmr@users.noreply.github.com>
2023-02-27 00:01:29 +0300

    Add files via upload

---
+++ translator.py
@@ -0,0 +1,52 @@
+#saatlik-dakikaklık artık ne isterseniz mailinize kelime + anlamı gönderecek program
+
+from smtplib import SMTP
+from datetime import datetime
+
+from googletrans import Translator, constants
+
+#simple mail transfer protokolu
+#Basit mail göndermek için kullanılır.
+try:
+    translator = Translator()
+    with open(r"C:\Users\bugra\OneDrive\Masaüstü\translate\wordlist.txt","r+",encoding="utf-8") as file:
+        lines = file.readlines()
+
+
+    for line in lines:
+        print(line)
+        while True:
+            print("2.nokta")
+            now = datetime.now()
+            print(now.second)
+            if now.second == 13:
+                break
+
+        if line == "":
+            print("4.nokta")
+            break
+        print("dwd")
+        translation = translator.translate(line,src="en",dest="tr")
+        print("qwdqwdwqdwdwq")
+        # mail mesaj bilgisi
+        subject = "dakikalık kelime"
+        message = "{}\t--\t{}".format(line,translation.text)
+        content = "Subject: {0}\n\n{1}".format(subject,message)
+
+        #gönderici hesap bilgileri
+        mymail = "bugra.ozdemir5834@gmail.com"
+        password = "aecbrcgoretejsis"        #google uygulama için şifre veriyor !!
+
+        #kime gonderilecek
+        sentto = "bugrareyiz12@gmail.com"
+
+        mail = SMTP("smtp.gmail.com",587)   #google smtp sunucusu
+        mail.ehlo()  #maile bağlandı
+        mail.starttls() #şifreli gönderdi mesajı
+        mail.login(mymail,password)
+        mail.sendmail(mymail,sentto,content.encode("utf-8"))  #encode türkçe karakterlerle sıkıntı çıkmaması için yazıldı.
+        print("success")
+
+    print("mail gonderme islemi basarılı")
+except Exception as e:
+    print("hata aldınız ! {}".format(e))
